ccc8412c63477c0ffcb256c0e3b711eceb1aaa8e
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-17 04:39:23 +0500

    Сгенерированны все модули в api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,15 +3,25 @@ import { UsersModule } from "./modules/users/users.module";
 import { PrismaModule } from "./modules/prisma/prisma.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
+import { CommentModule } from "./modules/comment/comment.module";
+import { UserCartModule } from "./modules/user-cart/user-cart.module";
+import { OrderModule } from "./modules/order/order.module";
+import { OrderItemModule } from './modules/order-item/order-item.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
 	imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
-    PrismaModule,
-    UsersModule, 
-    AuthModule
-  ],
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		PrismaModule,
+		UsersModule,
+		AuthModule,
+		CommentModule,
+		UserCartModule,
+		OrderModule,
+		OrderItemModule,
+		ProductModule,
+	],
 })
 export class AppModule {}
