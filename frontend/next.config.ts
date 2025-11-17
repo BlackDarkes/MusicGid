27d303cc522af66a8e86c8d0efe3181a50a0010e
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-18 04:26:23 +0500

    Версия next изменена 15 начал работать sass

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  images: {
+    domains: [process.env.API_URL!],
+  },
+  trailingSlash: true,
+  async rewrites() {
+    const apiPath = process.env.API_URL || "http://localhost:8000";
+
+    return [
+      {
+        source: "/api/:path",
+        destination: `${apiPath}/:path`,
+      },
+    ];
+  },
+  sassOptions: {
+    includePaths: [process.cwd()],
+    additionalData: `
+      @use "./src/app/styles/helpers/index.scss" as *;
+      @use "./src/app/styles/base/variables.scss" as *;
+    `,
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
