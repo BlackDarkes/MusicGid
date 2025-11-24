c637704201da2581c6ea129d7af4b2dbebceaf4d
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-24 20:31:11 +0500

    Почти сделанная бизнес логика для order

--- api/src/modules/order/order.service.ts
+++ api/src/modules/order/order.service.ts
@@ -1,4 +1,119 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+	constructor(private readonly prismaService: PrismaService) {}
+
+	async getAllOrders() {
+		return await this.prismaService.order.findMany({
+			include: {
+				user: {
+					select: {
+						id: true,
+						name: true,
+						email: true,
+						phone: true,
+					},
+				},
+				orderStatus: true,
+				paymentMethodsRel: true,
+				orderItems: {
+					include: {
+						product: {
+							select: {
+								id: true,
+								name: true,
+								image: true,
+								price: true,
+							},
+						},
+					},
+				},
+			},
+			orderBy: {
+				orderDate: "desc",
+			},
+		});
+	}
+
+	async getUserOrders(userId: string) {
+		const user = await this.prismaService.user.findUnique({
+			where: { id: userId },
+		});
+
+		if (!user) {
+			throw new NotFoundException("Пользователь не найден!");
+		}
+
+		return await this.prismaService.order.findMany({
+			where: {
+				userId,
+			},
+			include: {
+				orderStatus: true,
+				paymentMethodsRel: true,
+				orderItems: {
+					include: {
+						product: {
+							select: {
+								id: true,
+								name: true,
+								image: true,
+								price: true,
+							},
+						},
+					},
+				},
+			},
+			orderBy: {
+				orderDate: "desc",
+			},
+		});
+	}
+
+	async getOrderById(orderId: string) {
+		const order = await this.prismaService.order.findUnique({
+			where: {
+				id: orderId,
+			},
+			include: {
+				user: {
+					select: {
+						id: true,
+						name: true,
+						email: true,
+						phone: true,
+						address: true,
+					},
+				},
+				orderStatus: true,
+				paymentMethodsRel: true,
+				orderItems: {
+					include: {
+						product: {
+							select: {
+								id: true,
+								name: true,
+								image: true,
+								price: true,
+								brands: true,
+								categories: true,
+							},
+						},
+					},
+				},
+			},
+		});
+
+    if (!order) {
+      throw new NotFoundException("Заказ не найден!");
+    }
+
+    return order;
+	}
+
+  async create() {
+    
+  }
+}
