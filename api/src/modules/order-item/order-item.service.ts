1d82b3c853228adf5ededcdf890fafa021655c7c
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-28 02:30:09 +0500

    Начало работы над order item (нужно немного логику поменять)

--- api/src/modules/order-item/order-item.service.ts
+++ api/src/modules/order-item/order-item.service.ts
@@ -1,4 +1,142 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
-export class OrderItemService {}
+export class OrderItemService {
+	constructor(private readonly prismaService: PrismaService) {}
+
+	async getAllOrderItem() {
+		return await this.prismaService.orderItem.findMany({
+			include: {
+				order: {
+					select: {
+						id: true,
+						orderNumber: true,
+						orderDate: true,
+						user: {
+							select: {
+								id: true,
+								name: true,
+								email: true,
+							},
+						},
+					},
+				},
+				product: {
+					select: {
+						id: true,
+						name: true,
+						image: true,
+						price: true,
+						brands: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+					},
+				},
+			},
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
+	}
+
+	async getOrderItemByOrder(orderId: string) {
+		const order = await this.prismaService.order.findUnique({
+			where: { id: orderId },
+		});
+
+		if (!order) {
+			throw new NotFoundException("Заказ не найден!");
+		}
+
+		return this.prismaService.orderItem.findMany({
+			where: {
+				orderId,
+			},
+			include: {
+				order: {
+					select: {
+						id: true,
+						orderNumber: true,
+						orderDate: true,
+					},
+				},
+				product: {
+					select: {
+						id: true,
+						name: true,
+						image: true,
+						price: true,
+						brands: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+					},
+				},
+			},
+			orderBy: {
+				createdAt: "asc",
+			},
+		});
+	}
+
+	async getOrderItemById(orderItemId: string) {
+		const orderItem = await this.prismaService.orderItem.findUnique({
+			where: {
+				id: orderItemId,
+			},
+			include: {
+				order: {
+					select: {
+						id: true,
+						orderNumber: true,
+						orderDate: true,
+						totalAmount: true,
+						user: {
+							select: {
+								id: true,
+								name: true,
+								email: true,
+								phone: true,
+							},
+						},
+					},
+				},
+				product: {
+					select: {
+						id: true,
+						name: true,
+						image: true,
+						price: true,
+						brands: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+						categories: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+						count: true,
+					},
+				},
+			},
+		});
+
+		if (!orderItem) {
+			throw new NotFoundException("Товар из заказа не найден!");
+		}
+
+		return orderItem;
+	}
+
+  
+}
