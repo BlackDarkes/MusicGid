8c747a2b455e549855c86faa62d0a414cb1711a4
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-28 01:33:34 +0500

    Сделан контроллер для order(Базовый)

--- api/src/modules/order/order.controller.ts
+++ api/src/modules/order/order.controller.ts
@@ -1,7 +1,103 @@
-import { Controller } from '@nestjs/common';
-import { OrderService } from './order.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Patch,
+	Post,
+} from "@nestjs/common";
+import { OrderService } from "./order.service";
+import { CreateOrderDto } from "./common/dto/order.dto";
 
-@Controller('order')
+@Controller("order")
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+	constructor(private readonly orderService: OrderService) {}
+
+	@Get("")
+	@HttpCode(200)
+	async getAllOrders() {
+		const orders = await this.orderService.getAllOrders();
+
+		return {
+			orders,
+		};
+	}
+
+	@Get("user/:id")
+	@HttpCode(200)
+	async getAllUserOrders(@Param("id") userId: string) {
+		const userOrders = await this.orderService.getUserOrders(userId);
+
+		return {
+			orders: userOrders,
+		};
+	}
+
+	@Get(":id")
+	@HttpCode(200)
+	async getOrderById(@Param("id") orderId: string) {
+		const order = await this.orderService.getOrderById(orderId);
+
+		return {
+			order,
+		};
+	}
+
+	@Post("create")
+	@HttpCode(201)
+	async create(@Body() orderData: CreateOrderDto) {
+		await this.orderService.create(orderData);
+
+		return {
+			message: "Заказ сделан!",
+		};
+	}
+
+	@Patch(":id/status")
+	@HttpCode(201)
+	async updateStatus(@Param("id") orderId: string, @Body() statusId: number) {
+		const updateOrderStatus = await this.orderService.updateOrderStatus(
+			orderId,
+			statusId,
+		);
+
+		return {
+			order: updateOrderStatus,
+		};
+	}
+
+	@Patch(":id/address")
+	@HttpCode(201)
+	async updateAddress(@Param("id") orderId: string, @Body() address: string) {
+		const updateOrderAddress = await this.orderService.updateOrderAddress(
+			orderId,
+			address,
+		);
+
+		return {
+			order: updateOrderAddress,
+		};
+	}
+
+	@Delete(":id")
+	@HttpCode(201)
+	async delete(@Param("id") orderId: string) {
+		await this.orderService.deleteOrder(orderId);
+
+		return {
+			message: "Заказ удален!",
+		};
+	}
+
+	@Get("statuses/all")
+	@HttpCode(200)
+	async getStatuses() {
+		const statuses = await this.orderService.getOrderStatuses();
+
+		return {
+			statuses,
+		};
+	}
 }
