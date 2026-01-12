703563b2080c4e2bd1cc8aa8cbad8a84db80edd8
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-01-13 04:53:45 +0500

    Проверен Shadcn

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
   server: {
     port: 8080,
@@ -19,13 +22,13 @@ export default defineConfig({
   preview: { port: 8080 },
   resolve: {
     alias: {
-      "@/app/": "./src/app/",
-      "@/pages/": "./src/pages/",
-      "@/widgets/": "./src/widgets/",
-      "@/features/": "./src/features/",
-      "@/entities/": "./src/entities/",
-      "@/shared/": "./src/shared/",
-      "@/libs/": "./src/libs/",
-    }
-  }
-})
+      "@/app": resolve(__dirname, "./src/app/"),
+      "@/pages": "./src/pages/",
+      "@/widgets": "./src/widgets/",
+      "@/features": "./src/features/",
+      "@/entities": "./src/entities/",
+      "@/shared": resolve(__dirname, "./src/shared/"),
+      "@/libs": "./src/libs/",
+    },
+  },
+});
